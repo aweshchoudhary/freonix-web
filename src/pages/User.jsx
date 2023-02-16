ce2989f48517b7d92d12a89b02dbc8c74ef2899c
Awesh Choudhary <aweshchoudhary7@gmail.com>
2023-02-16 23:21:01 +0530

    developement

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -4,15 +4,20 @@ import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../store/auth/authSlice";
 import { useEffect, useRef, useState } from "react";
 import Loading from "../components/Loading";
-
-// import refreshUser from "../functions/refreshUser";
 import useRefreshUser from "../hooks/useRefreshUser";
-import updateAvatar from "../functions/updateAvatar";
-import deleteCover from "../functions/deleteCover";
-import updateCover from "../functions/updateCover";
-import getUserPosts from "../functions/getUserPosts";
-import followUser from "../functions/followUser";
-import unfollowUser from "../functions/unfollowUser";
+import { deleteObject } from "firebase/storage";
+import {
+  collection,
+  deleteField,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
+import { db } from "../config/firebase";
+import { toast } from "react-toastify";
 
 const User = () => {
   const { userid } = useParams();
@@ -31,30 +36,198 @@ const User = () => {
   function logoutUser() {
     dispatch(logout());
   }
+  function checkIsFollowed() {
+    if (data?.followers) {
+      const isExists = data.followers.indexOf(loggedUserId);
+      if (isExists > -1) {
+        setFollowed(true);
+      } else {
+        setFollowed(false);
+      }
+    }
+  }
+  async function deleteAvatar() {
+    const avatarImgRef = ref(storage, data.avatar.split("o/")[1]);
+    deleteObject(avatarImgRef)
+      .then(async () => {
+        const userRef = doc(db, "users", userid);
+        await updateDoc(userRef, {
+          avatar: deleteField(),
+        })
+          .then(() => {
+            toast.success("Avatar Deleted Successfully");
+          })
+          .catch((err) => toast.error(err.message));
+      })
+      .catch((err) => toast.error(err.message));
+
+    refreshUser();
+  }
+  async function deleteCover() {
+    const coverImgRef = ref(storage, cover.split("o/")[1]);
+    deleteObject(coverImgRef)
+      .then(async () => {
+        const userRef = doc(db, "users", userid);
+        await updateDoc(userRef, {
+          cover: deleteField(),
+        })
+          .then(() => {
+            toast.success("Cover Deleted Successfully");
+          })
+          .catch((err) => toast.error(err.message));
+      })
+      .catch((err) => toast.error(err.message));
+
+    refreshUser();
+  }
+  async function followUser() {
+    try {
+      if (data.id !== loggedUserId) {
+        const userRef = doc(db, "users", data.id);
+        const loggedUserRef = doc(db, "users", loggedUserId);
+        const getLoggedUser = await getDoc(loggedUserRef);
+        const loggedUserData = getLoggedUser.data();
+
+        const addFollower = data.followers
+          ? [...data.followers, loggedUserId]
+          : [loggedUserId];
+
+        const addFollowing = loggedUserData.followings
+          ? [...loggedUserData.followings, data.id]
+          : [data.id];
+
+        await updateDoc(userRef, {
+          followers: addFollower,
+        }).then(async () => {
+          await updateDoc(loggedUserRef, {
+            followings: addFollowing,
+          });
+        });
+      }
+    } catch (err) {
+      toast.error(err.message);
+      console.log(err);
+    } finally {
+      checkIsFollowed();
+    }
+
+    refreshUser();
+  }
+  async function unfollowUser() {
+    try {
+      if (data?.followers) {
+        const userRef = doc(db, "users", data.id);
+        const loggedUserRef = doc(db, "users", loggedUserId);
+        const getLoggedUser = await getDoc(loggedUserRef);
+        const loggedUserData = getLoggedUser.data();
+        const index = data.followers.indexOf(loggedUserId);
+        const findex = loggedUserData.followings.indexOf(data.id);
+
+        if (findex > -1) {
+          loggedUserData.followings.splice(index, 1);
+          await updateDoc(loggedUserRef, {
+            followings: loggedUserData.followings,
+          });
+        }
+        if (index > -1) {
+          data.followers.splice(index, 1);
+          await updateDoc(userRef, {
+            followers: data.followers,
+          });
+        }
+      }
+    } catch (err) {
+      toast.error(err.message);
+      console.log(err);
+    } finally {
+      checkIsFollowed();
+    }
+
+    refreshUser();
+  }
+  async function getUserPosts() {
+    const q = query(collection(db, "posts"), where("userid", "==", userid));
+    await getDocs(q).then((e) => {
+      e.forEach((post) => {
+        setPosts((prev) => [...prev, post.id]);
+      });
+    });
+  }
+  async function updateAvatar() {
+    let imgPath;
+    const [file] = avatarRef.current.files;
+    if (file.size > 1048576) {
+      return toast.error("File size limit is 1MB");
+    }
+    if (data?.avatar) {
+      await deleteAvatar(userid, data);
+    }
+    if (file) {
+      const avatarImgRef = ref(storage, Date.now() + "-" + file.name);
+      await uploadBytes(avatarImgRef, file)
+        .then((e) => {
+          imgPath =
+            "https://firebasestorage.googleapis.com/v0/b/twitter-clone-362d5.appspot.com/o/" +
+            e.metadata.fullPath;
+        })
+        .catch((err) => toast.error(err.message));
+      const avatarDataRef = doc(db, "users", userid);
+      await updateDoc(avatarDataRef, {
+        avatar: imgPath,
+      })
+        .then(() => {
+          toast.success("Avatar Updated");
+        })
+        .catch((err) => toast.error(err.message));
+    }
+
+    refreshUser();
+  }
+  async function updateCover() {
+    let imgPath;
+    const [file] = coverRef.current.files;
+    if (file.size > 1048576) {
+      return toast.error("File size limit is 1MB");
+    }
+    if (data?.cover) {
+      await deleteCover(userid, data);
+    }
+    if (file) {
+      const coverImgRef = ref(storage, Date.now() + "-" + file.name);
+      await uploadBytes(coverImgRef, file)
+        .then((e) => {
+          imgPath =
+            "https://firebasestorage.googleapis.com/v0/b/twitter-clone-362d5.appspot.com/o/" +
+            e.metadata.fullPath;
+        })
+        .catch((err) => toast.error(err.message));
+      const coverDataRef = doc(db, "users", userid);
+      await updateDoc(coverDataRef, {
+        cover: imgPath,
+      })
+        .then(() => {
+          toast.success("Cover Updated");
+        })
+        .catch((err) => toast.error(err.message));
+    }
+
+    refreshUser();
+  }
   const isMounted = useRef(false);
+
   useEffect(() => {
     refreshUser(userid, setData, setError, setLoading);
-    isMounted.current && getUserPosts(userid, setPosts);
+    // isMounted.current && getUserPosts(userid, setPosts);
     return () => {
       isMounted.current = true;
     };
   }, [userid]);
 
   useEffect(() => {
-    function checkIsFollowed() {
-      if (data?.followers) {
-        const isExists = data.followers.indexOf(loggedUserId);
-        if (isExists > -1) {
-          setFollowed(true);
-        } else {
-          setFollowed(false);
-        }
-      }
-    }
     checkIsFollowed();
-  }, [data]);
+  }, [data.followers]);
 
-  return !loading && data && !error ? (
+  return data && !error ? (
     <>
       <section className="header border-b pb-5 mb-5 bg-white">
         <div className="header relative">
